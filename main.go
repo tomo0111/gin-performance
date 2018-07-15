e308135fe06c5ac273a51584d0e30aa698d6691d
tomo0111 <tohito0430@gmail.com>
2018-07-15 21:05:27 +0800

    impl main method

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/tomo0111/gin-performance/controller"
+	"github.com/tomo0111/gin-performance/common"
 )
 
 func main() {
+	common.InitDB()
 	r := gin.Default()
 	r.GET("/", controller.HelloController)
+	r.GET("/items", controller.ItemController)
 	r.Run(":8080")
 }
 
